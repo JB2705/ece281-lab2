f5299ea0e4afad0c6bb177298b11f407a8e7d690
JB2705 <c27jonathan.barnett@afacademy.af.edu>
2025-02-24 11:09:16 -0700

    sevenseg_decoder

--- src/sevenseg_decoder.vhd
+++ src/sevenseg_decoder.vhd
@@ -39,6 +39,41 @@ end sevenseg_decoder;
 architecture Behavioral of sevenseg_decoder is
 
 begin
-
+--lay out all the rows from truth table, output when input
+with i_Hex select
+o_seg_n <=  "1000000" when "0000",
+            --1
+            "1111001" when "0001",
+            --2
+            "0100100" when "0010",
+            --3
+            "0110000" when "0011",
+            --4
+            "0011001" when "0100",
+            --5
+            "0010010" when "0101",
+            --6
+            "0000010" when "0110",
+            --7
+            "1111000" when "0111",
+            --8
+            "0000000" when "1000",
+            --9
+            "0011000" when "1001",
+            --A
+            "0001000" when "1010",
+            --B
+            "0000011" when "1011",
+            --C
+            "0100111" when "1100",
+            --D
+            "0100001" when "1101",
+            --E
+            "0000110" when "1110",
+            --F
+            "0001110" when "1111",
+            
+            --turn display off when else
+            "11111111" when others;
 
 end Behavioral;
